babfbdecda62e365b1724e6300988a81e397eda4
ChaTheDogge <95463244+ChaTheDogge@users.noreply.github.com>
2024-11-02 08:16:47 +0800

    Added a dash mechanic

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,9 +1,14 @@
 extends CharacterBody2D
 
-const gravity = 500
-var jump_strength = 500
+const gravity = 800
+var jump_strength = 600
 var speed = 400
-var friction = 1.2
+
+# Checks if the dash cooldown is over
+var dash_cooldown_over = true
+
+# Boolean for checking if the player is facing left or not
+var left = false
 
 func _process(delta: float) -> void:
 	pass
@@ -13,6 +18,7 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func get_input(delta: float):
+	# Gets rid of acceleration
 	velocity.x = 0
 	
 	if Input.is_action_pressed("jump") and is_on_floor():
@@ -21,11 +27,31 @@ func get_input(delta: float):
 	if Input.is_action_pressed("left"):
 		if velocity.x > -400:
 			velocity.x -= speed
+			left = true
 	elif Input.is_action_pressed("right"):
 		if velocity.x < 400:
 			velocity.x += speed
-	
+			left = false
+
+	if Input.is_action_just_pressed("dash"):
+		dash()
 
 	# Gravity
 	if not is_on_floor():
 		velocity.y += gravity * delta
+
+func dash():
+	if left:
+		if dash_cooldown_over == false:
+			return
+		velocity.x -= 2000 * 6
+		dash_cooldown_over = false
+		await get_tree().create_timer(0.5).timeout
+		dash_cooldown_over = true
+	else:
+		if dash_cooldown_over == false:
+			return
+		velocity.x += 2000 * 6
+		dash_cooldown_over = false
+		await get_tree().create_timer(0.5).timeout
+		dash_cooldown_over = true
