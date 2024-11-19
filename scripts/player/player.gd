9395a7fbfe0462f5d70d5e61275550abe67fa37e
Prince Lawrence Jacinto <jacinto.princelawrence@gmail.com>
2024-11-20 00:45:17 +0800

    fixes dash, bugs, and added crouch

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,19 +1,20 @@
 extends CharacterBody2D
 
-const gravity = 400
-var dash_speed = 500
-var jump_strength = 200
-var speed = 100
+const gravity = 500
+var dash_speed = 700
+var jump_strength = 250
+var speed = 200
 var is_dashing = false 
 var can_dash = true
 var is_facing_left = false
 var is_jumping = false
+var is_crouching = false
+var crouch_speed = 100
 
 func _physics_process(delta: float) -> void:
-	
+	print($AnimatedSprite2D.frame)
 	get_input(delta)
 	move_and_slide()
-	print($AnimatedSprite2D.animation)
 
 func get_input(delta: float):
 	if is_dashing:
@@ -28,42 +29,62 @@ func get_input(delta: float):
 			$AnimatedSprite2D.play("idle_right")
 	
 	if direction:
-		velocity.x = direction * speed
+		if !is_crouching:
+			velocity.x = direction * speed
+		elif is_crouching:
+			velocity.x = direction * crouch_speed
 		if velocity.x > 0 and is_on_floor():
 			$AnimatedSprite2D.play("walk_right")
 			is_facing_left = false
 		elif velocity.x < 0 and is_on_floor():
 			$AnimatedSprite2D.play("walk_left")
 			is_facing_left = true
-
-	# Jump
-	if is_on_floor() and Input.is_action_pressed("jump"):
-			jump()
-			
-
-
 	# Gravity
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		if velocity.x > 0:
+			is_facing_left = false
+		elif velocity.x < 0: 
+			is_facing_left = true
+		if is_facing_left:
+			$AnimatedSprite2D.play("jump_left")
+		else:
+			$AnimatedSprite2D.play("jump_right")
 		
 	if Input.is_action_just_pressed("dash"):
 		dash()
+	if Input.is_action_pressed("crouch"):
+		crouch()
+	elif Input.is_action_just_released("crouch"):
+		is_crouching = false
+	if Input.is_action_pressed("jump"):
+		jump()
 
 func jump():
-	is_dashing = false
-	is_jumping = true
-	velocity.y -= jump_strength
-	$AnimatedSprite2D.play("jump_right")
-	print("jumping")
+	if is_on_floor():
+		is_dashing = false
+		is_jumping = true
+		velocity.y -= jump_strength
+		
+		if is_facing_left:
+			$AnimatedSprite2D.play("jump_left")
+			print("jumping left")
+		else:
+			$AnimatedSprite2D.play("jump_right")
+			print("jumping right")
+			
+			
+func crouch():
+	if is_on_floor():
+		is_crouching = true
+		$AnimatedSprite2D.play("crouch_left" if is_facing_left and velocity.x < 0 else "crouch_right")
 
 func dash():
 	if can_dash:
 		is_dashing = true
-
-		# Apply dash speed
+		velocity.y = 0
 		velocity.x = -dash_speed if is_facing_left else dash_speed
 		$AnimatedSprite2D.play("dash_left" if is_facing_left else "dash_right")
-		
 		# Start dash timer
 		$DashDuration.start(0.3)
 
@@ -72,7 +93,8 @@ func _on_dash_duration_timeout() -> void:
 	can_dash = false
 	velocity.x = 0  # Stop horizontal movement after dash
 	$DashCooldown.start(1)
-	print("dash duration finished")
+	if !is_on_floor():
+		$AnimatedSprite2D.play("falling_down")
 
 func _on_dash_cooldown_timeout() -> void:
 	can_dash = true
