f2f3345dbe29423e0daf93acbc54d38d94f81ef7
Prince Lawrence Jacinto <jacinto.princelawrence@gmail.com>
2024-12-31 10:46:44 +0800

    rewrite player movements/added state machine

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,101 +1,125 @@
 extends CharacterBody2D
 
-const gravity = 500
-var dash_speed = 700
-var jump_strength = 250
+enum player_state {
+	idle,
+	walk,
+	jump,
+	fall,
+	dash
+}
+
+var current_state = player_state.idle
+var gravity = 700
+var jump_force = -350
 var speed = 200
-var is_dashing = false 
+var air_speed = 150  # Reduced speed while in the air
+var dash_speed = 550
 var can_dash = true
-var is_facing_left = false
-var is_jumping = false
-var is_crouching = false
-var crouch_speed = 100
 
 func _physics_process(delta: float) -> void:
-	print($AnimatedSprite2D.frame)
-	get_input(delta)
+	match current_state:
+		player_state.idle:
+			handle_idle()
+		player_state.walk:
+			handle_walk()
+		player_state.jump:
+			handle_jump()
+		player_state.fall:
+			handle_fall()
+		player_state.dash:
+			handle_dash()
+
+	# Apply gravity
+	if !is_on_floor():
+		velocity.y += gravity * delta
+
+	# Apply movement
 	move_and_slide()
 
-func get_input(delta: float):
-	if is_dashing:
-		return  # Skip normal movement during dash
+	print("State: ", current_state)
+	print("is on floor: ", is_on_floor())
+	print($AnimatedSprite2D.flip_h)
 
+func handle_idle():
+	$AnimatedSprite2D.play("idle")
+	velocity.x = 0  # Stop horizontal movement
+	if Input.is_action_pressed("right") or Input.is_action_pressed("left"):
+		current_state = player_state.walk
+	elif Input.is_action_pressed("jump") and is_on_floor():
+		velocity.y = jump_force  # Apply jump force
+		current_state = player_state.jump
+	elif Input.is_action_pressed("dash"):
+		if can_dash:
+			current_state = player_state.dash
+		else:
+			pass
+
+# Walk state behavior
+func handle_walk():
+	$AnimatedSprite2D.play("walk")
+	var direction = Input.get_axis("left", "right")  # -1 for left, 1 for right
+	velocity.x = direction * speed
+	if !Input.is_action_pressed("right") and !Input.is_action_pressed("left"):
+		current_state = player_state.idle
+	elif Input.is_action_pressed("jump") and is_on_floor():
+		velocity.y = jump_force  # Apply jump force
+		current_state = player_state.jump
+	elif Input.is_action_pressed("dash"):
+		if can_dash:
+			current_state = player_state.dash
+		else:
+			pass
+
+	if direction < 0:  # Moving left
+		$AnimatedSprite2D.flip_h = true
+	elif direction > 0:  # Moving right
+		$AnimatedSprite2D.flip_h = false
+
+# Jump state behavior
+func handle_jump():
+	var direction = Input.get_axis("left", "right")  # -1 for left, 1 for right
+	$AnimatedSprite2D.play("jump")
+	handle_air_movement()
+	if velocity.y > 0:  # If moving downward
+		current_state = player_state.fall
+
+
+# Fall state behavior
+func handle_fall():
+	$AnimatedSprite2D.play("fall")
+	handle_air_movement()
+	if is_on_floor():  # Transition back to idle when landing
+		current_state = player_state.idle
+
+# Handle movement while in the air (jump or fall)
+func handle_air_movement():
 	var direction = Input.get_axis("left", "right")
-	velocity.x = 0
-	if is_on_floor():
-		if is_facing_left and !direction:
-			$AnimatedSprite2D.play("idle_left")
-		elif !is_facing_left and !direction:
-			$AnimatedSprite2D.play("idle_right")
+	velocity.x = direction * air_speed
+	if direction < 0:  # Moving left
+		$AnimatedSprite2D.flip_h = true
+	elif direction > 0:  # Moving right
+		$AnimatedSprite2D.flip_h = false
 	
-	if direction:
-		if !is_crouching:
-			velocity.x = direction * speed
-		elif is_crouching:
-			velocity.x = direction * crouch_speed
-		if velocity.x > 0 and is_on_floor():
-			$AnimatedSprite2D.play("walk_right")
-			is_facing_left = false
-		elif velocity.x < 0 and is_on_floor():
-			$AnimatedSprite2D.play("walk_left")
-			is_facing_left = true
-	# Gravity
-	if not is_on_floor():
-		velocity.y += gravity * delta
-		if velocity.x > 0:
-			is_facing_left = false
-		elif velocity.x < 0: 
-			is_facing_left = true
-		if is_facing_left:
-			$AnimatedSprite2D.play("jump_left")
-		else:
-			$AnimatedSprite2D.play("jump_right")
-		
-	if Input.is_action_just_pressed("dash"):
-		dash()
-	if Input.is_action_pressed("crouch"):
-		crouch()
-	elif Input.is_action_just_released("crouch"):
-		is_crouching = false
-	if Input.is_action_pressed("jump"):
-		jump()
+	if Input.is_action_pressed("dash"):
+		current_state = player_state.dash
 
-func jump():
-	if is_on_floor():
-		is_dashing = false
-		is_jumping = true
-		velocity.y -= jump_strength
-		
-		if is_facing_left:
-			$AnimatedSprite2D.play("jump_left")
-			print("jumping left")
-		else:
-			$AnimatedSprite2D.play("jump_right")
-			print("jumping right")
-			
-			
-func crouch():
-	if is_on_floor():
-		is_crouching = true
-		$AnimatedSprite2D.play("crouch_left" if is_facing_left and velocity.x < 0 else "crouch_right")
-
-func dash():
-	if can_dash:
-		is_dashing = true
-		velocity.y = 0
-		velocity.x = -dash_speed if is_facing_left else dash_speed
-		$AnimatedSprite2D.play("dash_left" if is_facing_left else "dash_right")
-		# Start dash timer
+func handle_dash():
+	$AnimatedSprite2D.play("dash")
+	velocity.y = 0
+	if $DashDuration.is_stopped():
 		$DashDuration.start(0.3)
+	if $AnimatedSprite2D.flip_h:
+		velocity.x = -dash_speed
+	else:
+		velocity.x = dash_speed
 
 func _on_dash_duration_timeout() -> void:
-	is_dashing = false
+	print("Dash timer finished")  # Logs when the timer ends
+	current_state = player_state.idle
 	can_dash = false
-	velocity.x = 0  # Stop horizontal movement after dash
-	$DashCooldown.start(1)
-	if !is_on_floor():
-		$AnimatedSprite2D.play("falling_down")
+	$DashCooldown.start(3)
+
 
 func _on_dash_cooldown_timeout() -> void:
-	can_dash = true
 	print("dash cooldowned")
+	can_dash = true
